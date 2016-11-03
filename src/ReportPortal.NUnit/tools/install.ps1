d4f9987acbb1ba9a0b7f517ba4e081413e643636
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2016-11-04 02:36:15 +0300

    Added exception to gitignore

---
+++ src/ReportPortal.NUnit/tools/install.ps1
@@ -0,0 +1,14 @@
+﻿# Runs every time a package is installed in a project
+
+param($installPath, $toolsPath, $package, $project)
+
+# $installPath is the path to the folder where the package is installed.
+# $toolsPath is the path to the tools directory in the folder where the package is installed.
+# $package is a reference to the package object.
+# $project is a reference to the project the package was installed to.
+
+$file1 = $project.ProjectItems.Item("ReportPortal.NUnit.dll.config")
+
+# set 'Copy To Output Directory' to 'Copy if newer'
+$copyToOutput1 = $file1.Properties.Item("CopyToOutputDirectory")
+$copyToOutput1.Value = 2
