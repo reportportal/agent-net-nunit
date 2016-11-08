896391750b2cb113fee9363d27938177bda81322
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2016-11-08 19:13:33 +0300

    Renamed addin

--- src/ReportPortal.NUnit/ReportPortalListener.cs
+++ src/ReportPortal.NUnit/ReportPortalListener.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ReportPortal.NUnit
+namespace ReportPortal.NUnitAddin
 {
     [Extension]
     public class ReportPortalListener : ITestEventListener
