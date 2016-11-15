b0dcf1b3890469092574c6e84539b38aff57d744
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2016-11-15 22:45:06 +0300

    Report properties for suites as tags

--- src/ReportPortal.NUnitExtension/ReportPortalListener.Suite.cs
+++ src/ReportPortal.NUnitExtension/ReportPortalListener.Suite.cs
@@ -3,6 +3,7 @@ using ReportPortal.Client.Requests;
 using ReportPortal.NUnitExtension.EventArguments;
 using ReportPortal.Shared;
 using System;
+using System.Collections.Generic;
 using System.Xml;
 
 namespace ReportPortal.NUnitExtension
@@ -72,43 +73,69 @@ namespace ReportPortal.NUnitExtension
 
         private void FinishSuite(XmlDocument xmlDoc)
         {
-            var id = xmlDoc.SelectSingleNode("/*/@id").Value;
-            var result = xmlDoc.SelectSingleNode("/*/@result").Value;
-            var parentId = xmlDoc.SelectSingleNode("/*/@parentId");
-
-            // at the end of execution nunit raises 2 the same events, we need only that which has 'parentId' xml tag
-            if (parentId != null)
+            try
             {
-                if (!_suitesFlow[id].Canceled)
+                var id = xmlDoc.SelectSingleNode("/*/@id").Value;
+                var result = xmlDoc.SelectSingleNode("/*/@result").Value;
+                var parentId = xmlDoc.SelectSingleNode("/*/@parentId");
+
+                // at the end of execution nunit raises 2 the same events, we need only that which has 'parentId' xml tag
+                if (parentId != null)
                 {
-                    var finishSuiteRequest = new FinishTestItemRequest
+                    if (!_suitesFlow[id].Canceled)
                     {
-                        EndTime = DateTime.UtcNow,
-                        Status = _statusMap[result]
-                    };
+                        // adding properties to suite
+                        var properties = xmlDoc.SelectNodes("//properties/property");
+                        if (properties != null)
+                        {
+                            var updateSuiteRequest = new UpdateTestItemRequest()
+                            {
+                                Description = "",
+                                Tags = new List<string>()
+                            };
 
-                    var eventArg = new TestItemFinishedEventArgs(Bridge.Service, finishSuiteRequest, result, _suitesFlow[id].Id);
+                            foreach (XmlNode property in properties)
+                            {
+                                updateSuiteRequest.Tags.Add(string.Format("{0}: {1}", property.Attributes["name"].Value, property.Attributes["value"].Value));
+                            }
 
-                    try
-                    {
-                        if (BeforeSuiteFinished != null) BeforeSuiteFinished(this, eventArg);
-                    }
-                    catch (Exception exp)
-                    {
-                        Console.WriteLine("Exception was thrown in 'BeforeSuiteFinished' subscriber." + Environment.NewLine + exp);
-                    }
+                            Bridge.Service.UpdateTestItem(_suitesFlow[id].Id, updateSuiteRequest);
+                        }
 
-                    var message = Bridge.Service.FinishTestItem(_suitesFlow[id].Id, finishSuiteRequest).Info;
+                        var finishSuiteRequest = new FinishTestItemRequest
+                        {
+                            EndTime = DateTime.UtcNow,
+                            Status = _statusMap[result]
+                        };
 
-                    try
-                    {
-                        if (AfterSuiteFinished != null) AfterSuiteFinished(this, new TestItemFinishedEventArgs(Bridge.Service, finishSuiteRequest, message, _suitesFlow[id].Id));
-                    }
-                    catch (Exception exp)
-                    {
-                        Console.WriteLine("Exception was thrown in 'AfterSuiteFinished' subscriber." + Environment.NewLine + exp);
+                        // finishing suite
+                        var eventArg = new TestItemFinishedEventArgs(Bridge.Service, finishSuiteRequest, result, _suitesFlow[id].Id);
+
+                        try
+                        {
+                            if (BeforeSuiteFinished != null) BeforeSuiteFinished(this, eventArg);
+                        }
+                        catch (Exception exp)
+                        {
+                            Console.WriteLine("Exception was thrown in 'BeforeSuiteFinished' subscriber." + Environment.NewLine + exp);
+                        }
+
+                        var message = Bridge.Service.FinishTestItem(_suitesFlow[id].Id, finishSuiteRequest).Info;
+
+                        try
+                        {
+                            if (AfterSuiteFinished != null) AfterSuiteFinished(this, new TestItemFinishedEventArgs(Bridge.Service, finishSuiteRequest, message, _suitesFlow[id].Id));
+                        }
+                        catch (Exception exp)
+                        {
+                            Console.WriteLine("Exception was thrown in 'AfterSuiteFinished' subscriber." + Environment.NewLine + exp);
+                        }
                     }
                 }
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine("ReportPortal exception was thrown." + Environment.NewLine + exception);
             }
         }
     }
