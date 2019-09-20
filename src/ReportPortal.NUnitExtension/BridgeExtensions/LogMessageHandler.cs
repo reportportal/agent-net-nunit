2f13f7dd8ce52ff8ac73cc9eca9810445b981bea
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2019-09-20 16:48:49 +0300

    Help nunit runner to resolve nunit.framework assembly for LogHandler

--- src/ReportPortal.NUnitExtension/BridgeExtensions/LogMessageHandler.cs
+++ src/ReportPortal.NUnitExtension/BridgeExtensions/LogMessageHandler.cs
@@ -1,11 +1,30 @@
 ﻿using ReportPortal.Client.Requests;
 using ReportPortal.Client.Converters;
 using ReportPortal.Shared.Extensibility;
+using System;
+using System.Linq;
 
 namespace ReportPortal.NUnitExtension.BridgeExtensions
 {
     public class LogMessageHandler : ILogHandler
     {
+        static LogMessageHandler()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        private static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            // since this assembly has deep link to "nunit.framework", use any already loaded "nunit.framework" assembly from test app domain
+
+            if (args.Name.Contains("nunit.framework"))
+            {
+                return AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().Name == "nunit.framework");
+            }
+
+            return null;
+        }
+
         public int Order => 100;
 
         public bool Handle(AddLogItemRequest logRequest)
