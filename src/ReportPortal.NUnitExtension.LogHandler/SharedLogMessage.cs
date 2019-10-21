909bfe44b7af84f652a28a09e435e8870c8cdabc
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2019-10-21 23:05:32 +0300

    Move log handler to separate dll without dependencies for nunit engine

---
+++ src/ReportPortal.NUnitExtension.LogHandler/SharedLogMessage.cs
@@ -0,0 +1,72 @@
+﻿using ReportPortal.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportPortal.NUnitExtension.LogHandler
+{
+    /// <summary>
+    /// LogHandler serializes this class to be handled later in nunit extension
+    /// </summary>
+    [DataContract]
+    class SharedLogMessage
+    {
+        /// <summary>
+        /// ID of test item to add new logs.
+        /// </summary>
+        [DataMember]
+        public string TestItemId { get; set; }
+
+        /// <summary>
+        /// Date time of log item.
+        /// </summary>
+        [DataMember]
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// A level of log item.
+        /// </summary>
+        [DataMember]
+        public LogLevel Level = LogLevel.Info;
+
+        /// <summary>
+        /// Message of log item.
+        /// </summary>
+        [DataMember]
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Specify an attachment of log item.
+        /// </summary>
+        [DataMember]
+        public Attach Attach { get; set; }
+    }
+
+    [DataContract]
+    public class Attach
+    {
+        public Attach()
+        {
+
+        }
+
+        public Attach(string name, string mimeType, byte[] data)
+        {
+            Name = name;
+            MimeType = mimeType;
+            Data = data;
+        }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public byte[] Data { get; set; }
+
+        [DataMember]
+        public string MimeType { get; set; }
+    }
+}
--- src/ReportPortal.NUnitExtension/BridgeExtensions/LogMessageHandler.cs
+++ src/ReportPortal.NUnitExtension/BridgeExtensions/LogMessageHandler.cs
@@ -6,49 +6,5 @@ using System.Linq;
 
 namespace ReportPortal.NUnitExtension.BridgeExtensions
 {
-    public class LogMessageHandler : ILogHandler
-    {
-        static LogMessageHandler()
-        {
-            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
-        }
-
-        private static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            // since this assembly has deep link to "nunit.framework", use any already loaded "nunit.framework" assembly from test app domain
-
-            if (args.Name.Contains("nunit.framework"))
-            {
-                return AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().Name == "nunit.framework");
-            }
-
-            return null;
-        }
-
-        public int Order => 100;
-
-        public bool Handle(AddLogItemRequest logRequest)
-        {
-            var sharedMessage = new SharedLogMessage()
-            {
-                TestItemId = logRequest.TestItemId,
-                Time = logRequest.Time,
-                Text = logRequest.Text,
-                Level = logRequest.Level
-            };
-            if (logRequest.Attach != null)
-            {
-                sharedMessage.Attach = new Attach
-                {
-                    Name = logRequest.Attach.Name,
-                    MimeType = logRequest.Attach.MimeType,
-                    Data = logRequest.Attach.Data
-                };
-            }
-
-            NUnit.Framework.Internal.TestExecutionContext.CurrentContext.SendMessage("ReportPortal", ModelSerializer.Serialize<SharedLogMessage>(sharedMessage));
-
-            return true;
-        }
-    }
+    
 }
