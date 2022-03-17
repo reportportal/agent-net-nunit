ed0ebf46ab60c3d3de01503061edc056b10c06fa
Aleh Yanushkevich <Aleh_Yanushkevich2@epam.com>
2022-03-17 11:45:06 +0300

    Refactored ReportPortalListener.Suite file.

---
+++ src/ReportPortal.NUnitExtension/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using ReportPortal.Shared.Configuration;
+using System.Collections.Generic;
+
+namespace ReportPortal.NUnitExtension.Extensions
+{
+    internal static class ConfigurationExtensions
+    {
+        public static IEnumerable<string> GetRootNamespaces(this IConfiguration configuration, IEnumerable<string> defaultValue = null)
+        {
+            return configuration.GetValues<string>("rootNamespaces", defaultValue);
+        }
+    }
+}
--- src/ReportPortal.NUnitExtension/ReportPortalListener.Suite.cs
+++ src/ReportPortal.NUnitExtension/ReportPortalListener.Suite.cs
@@ -4,6 +4,7 @@ using ReportPortal.NUnitExtension.EventArguments;
 using ReportPortal.NUnitExtension.Extensions;
 using ReportPortal.Shared.Reporter;
 using System;
+using System.Globalization;
 using System.Linq;
 using System.Xml.Linq;
 using System.Xml.XPath;
@@ -20,163 +21,153 @@ namespace ReportPortal.NUnitExtension
 
         public static EventHandler<TestItemFinishedEventArgs> AfterSuiteFinished;
 
-        private void StartSuite(string report)
+        private void StartSuite(string report) => InvokeSafely(() => StartSuite(XElement.Parse(report)));
+
+        private void StartSuite(XElement xElement)
         {
-            var xElement = XElement.Parse(report);
+            var id = xElement.Attribute("id").Value;
+            var name = xElement.Attribute("name").Value;
+            var parentId = xElement.Attribute("parentId").Value;
 
-            try
+            var startTime = DateTime.UtcNow;
+
+            var startSuiteRequest = new StartTestItemRequest
             {
-                var type = xElement.Attribute("type").Value;
+                StartTime = startTime,
+                Name = name,
+                Type = TestItemType.Suite
+            };
 
-                var id = xElement.Attribute("id").Value;
-                var parentId = xElement.Attribute("parentId").Value;
-                var name = xElement.Attribute("name").Value;
-                var fullname = xElement.Attribute("fullname").Value;
-
-                var startTime = DateTime.UtcNow;
-
-                var startSuiteRequest = new StartTestItemRequest
-                {
-                    StartTime = startTime,
-                    Name = name,
-                    Type = TestItemType.Suite
-                };
-
-                var itemStartedEventArgs = new TestItemStartedEventArgs(_rpService, startSuiteRequest, null, report);
-
-                var rootNamespaces = Config.GetValues<string>("rootNamespaces", null);
-                if (rootNamespaces != null && rootNamespaces.Any(n => n == name))
-                {
-                    itemStartedEventArgs.Canceled = true;
-                }
-
-                if (!itemStartedEventArgs.Canceled)
-                {
-                    RiseEvent(BeforeSuiteStarted, itemStartedEventArgs, nameof(BeforeSuiteStarted));
-                }
-
-                if (!itemStartedEventArgs.Canceled)
-                {
-                    ITestReporter suiteReporter;
-
-                    if (!parentId.HasValue() || !_flowItems.ContainsKey(parentId))
-                    {
-                        suiteReporter = _launchReporter.StartChildTestReporter(startSuiteRequest);
-                    }
-                    else
-                    {
-                        var parentFlowItem = FindReportedParentFlowItem(parentId);
-                        if (parentFlowItem == null)
-                        {
-                            suiteReporter = _launchReporter.StartChildTestReporter(startSuiteRequest);
-                        }
-                        else
-                        {
-                            suiteReporter = parentFlowItem.TestReporter.StartChildTestReporter(startSuiteRequest);
-                        }
-                    }
-
-                    _flowItems[id] = new FlowItemInfo(id, parentId, FlowItemInfo.FlowType.Suite, name, suiteReporter, startTime);
-
-                    itemStartedEventArgs = new TestItemStartedEventArgs(_rpService, startSuiteRequest, suiteReporter, report);
-                    RiseEvent(AfterSuiteStarted, itemStartedEventArgs, nameof(AfterSuiteStarted));
-                }
-                else
-                {
-                    _flowItems[id] = new FlowItemInfo(id, parentId, FlowItemInfo.FlowType.Suite, name, null, startTime);
-                }
-            }
-            catch (Exception exception)
+            var report = xElement.ToString();
+            var beforeSuiteEventArgs = new TestItemStartedEventArgs(_rpService, startSuiteRequest, null, report)
             {
-                _traceLogger.Error("ReportPortal exception was thrown." + Environment.NewLine + exception);
+                Canceled = Config.GetRootNamespaces()?.Any(root => root == name) ?? false
+            };
+
+            if (!beforeSuiteEventArgs.Canceled)
+            {
+                RiseEvent(BeforeSuiteStarted, beforeSuiteEventArgs, nameof(BeforeSuiteStarted));
+                var suiteReporter = GetSuiteReporter(parentId, startSuiteRequest);
+
+                _flowItems[id] = new FlowItemInfo(
+                    id, parentId, FlowItemInfo.FlowType.Suite, name, suiteReporter, startTime);
+
+                beforeSuiteEventArgs.TestReporter = suiteReporter;
+                RiseEvent(AfterSuiteStarted, beforeSuiteEventArgs, nameof(AfterSuiteStarted));
             }
+            else
+            {
+                _flowItems[id] = new FlowItemInfo(id, parentId, FlowItemInfo.FlowType.Suite, name, null, startTime);
+            }
+        }
+
+        private void FinishSuite(string report) => InvokeSafely(() => FinishSuite(XElement.Parse(report)));
+
+        private void FinishSuite(XElement xElement)
+        {
+            var type = xElement.Attribute("type").Value;
+            var id = xElement.Attribute("id").Value;
+            var result = xElement.Attribute("result").Value;
+
+            var parentId = xElement.Attribute("parentId");
+            var duration = float.Parse(xElement.Attribute("duration").Value, CultureInfo.InvariantCulture);
+
+            // at the end of execution nunit raises 2 the same events, we need only that which has 'parentId' xml tag
+            if (parentId is null)
+            {
+                return;
+            }
+
+            var report = xElement.ToString();
+
+            if (!_flowItems.ContainsKey(id))
+            {
+                StartSuite(report);
+            }
+
+            if (!_flowItems.ContainsKey(id))
+            {
+                return;
+            }
+
+            var finishSuiteRequest = new FinishTestItemRequest
+            {
+                EndTime = _flowItems[id].StartTime.AddSeconds(duration),
+                Status = _statusMap[result]
+            };
+
+            HandleProperties(xElement, finishSuiteRequest);
+
+            var eventArgs = new TestItemFinishedEventArgs(
+                _rpService, finishSuiteRequest, _flowItems[id].TestReporter, report);
+
+            RiseEvent(BeforeSuiteFinished, eventArgs, nameof(BeforeSuiteFinished));
+
+            /* 
+                Understand whether finishing test item should be deferred.
+                Usually we need it to report stacktrace in case of OneTimeSetup method fails, and stacktrace is avalable later in "FinishSuite" method
+            */
+            if (IsShouldBeDeferred(xElement))
+            {
+                _flowItems[id].FinishTestItemRequest = finishSuiteRequest;
+                _flowItems[id].Report = report;
+                _flowItems[id].DeferredFinishAction = GetFinishSuiteAction();
+            }
+            else
+            {
+                var failurestacktrace = xElement.XPathSelectElement("//failure/stack-trace")?.Value;
+                GetFinishSuiteAction().Invoke(id, finishSuiteRequest, report, failurestacktrace);
+            }
+        }
+
+        private Action<string, FinishTestItemRequest, string, string> GetFinishSuiteAction()
+        {
+            return (id, request, report, parentStackTrace) =>
+            {
+                // find all defferred children test items to finish
+                var deferredFlowItems = _flowItems
+                    .Where(fi => fi.Value.ParentId == id && fi.Value.DeferredFinishAction != null)
+                    .Select(fi => fi.Value)
+                    .ToList();
+
+                foreach (var deferredFlowItem in deferredFlowItems)
+                {
+                    deferredFlowItem.DeferredFinishAction.Invoke(
+                        deferredFlowItem.Id,
+                        deferredFlowItem.FinishTestItemRequest,
+                        deferredFlowItem.Report,
+                        parentStackTrace);
+                }
+
+                var reporter = _flowItems[id].TestReporter;
+                reporter?.Finish(request);
+
+                var testFinishedEventArgs = new TestItemFinishedEventArgs(_rpService, request, reporter, report);
+                RiseEvent(AfterSuiteFinished, testFinishedEventArgs, nameof(AfterSuiteFinished));
+
+                _flowItems.Remove(id);
+            };
+        }
+
+        private ITestReporter GetSuiteReporter(string parentId, StartTestItemRequest request)
+        {
+            if (!parentId.HasValue() || !_flowItems.ContainsKey(parentId))
+            {
+                return _launchReporter.StartChildTestReporter(request);
+            }
+
+            var parentFlowItem = FindReportedParentFlowItem(parentId);
+
+            return parentFlowItem is null
+                ? _launchReporter.StartChildTestReporter(request)
+                : parentFlowItem.TestReporter.StartChildTestReporter(request);
         }
 
         private FlowItemInfo FindReportedParentFlowItem(string id)
         {
-            if (_flowItems[id].TestReporter != null)
-            {
-                return _flowItems[id];
-            }
-            else if (_flowItems[id].ParentId.HasValue())
-            {
-                return FindReportedParentFlowItem(_flowItems[id].ParentId);
-            }
-            else return null;
-        }
-
-        private void FinishSuite(string report)
-        {
-            var xElement = XElement.Parse(report);
-
-            try
-            {
-                var type = xElement.Attribute("type").Value;
-
-                var id = xElement.Attribute("id").Value;
-                var result = xElement.Attribute("result").Value;
-                var parentId = xElement.Attribute("parentId");
-                var duration = float.Parse(xElement.Attribute("duration").Value, System.Globalization.CultureInfo.InvariantCulture);
-
-                // at the end of execution nunit raises 2 the same events, we need only that which has 'parentId' xml tag
-                if (parentId != null)
-                {
-                    if (!_flowItems.ContainsKey(id))
-                    {
-                        StartSuite(report);
-                    }
-
-                    if (_flowItems.ContainsKey(id))
-                    {
-                        // finishing suite
-                        var finishSuiteRequest = new FinishTestItemRequest
-                        {
-                            EndTime = _flowItems[id].StartTime.AddSeconds(duration),
-                            Status = _statusMap[result]
-                        };
-
-                        HandleProperties(xElement, finishSuiteRequest);
-
-                        var itemFinishedEventArgs = new TestItemFinishedEventArgs(_rpService, finishSuiteRequest, _flowItems[id].TestReporter, report);
-                        RiseEvent(BeforeSuiteFinished, itemFinishedEventArgs, nameof(BeforeSuiteFinished));
-
-                        Action<string, FinishTestItemRequest, string, string> finishSuiteAction = (__id, __finishSuiteRequest, __report, __parentstacktrace) =>
-                        {
-                            // find all defferred children test items to finish
-                            var deferredFlowItems = _flowItems.Where(fi => fi.Value.ParentId == __id && fi.Value.DeferredFinishAction != null).Select(fi => fi.Value).ToList();
-                            foreach (var deferredFlowItem in deferredFlowItems)
-                            {
-                                deferredFlowItem.DeferredFinishAction.Invoke(deferredFlowItem.Id, deferredFlowItem.FinishTestItemRequest, deferredFlowItem.Report, __parentstacktrace);
-                            }
-
-                            _flowItems[__id].TestReporter?.Finish(__finishSuiteRequest);
-
-                            var args = new TestItemFinishedEventArgs(_rpService, __finishSuiteRequest, _flowItems[__id].TestReporter, __report);
-                            RiseEvent(AfterSuiteFinished, args, nameof(AfterSuiteFinished));
-
-                            _flowItems.Remove(__id);
-                        };
-
-                        // understand whether finishing test suite should be defferred. Usually we need it to report stacktrace in case of OneTimeSetup method fails, and stacktrace is avalable later in "FinishSuite" method
-                        if (xElement.Attribute("site")?.Value == "Parent")
-                        {
-                            _flowItems[id].FinishTestItemRequest = finishSuiteRequest;
-                            _flowItems[id].Report = report;
-                            _flowItems[id].DeferredFinishAction = finishSuiteAction;
-                        }
-                        else
-                        {
-                            var failurestacktrace = xElement.XPathSelectElement("//failure/stack-trace")?.Value;
-
-                            finishSuiteAction.Invoke(id, finishSuiteRequest, report, failurestacktrace);
-                        }
-                    }
-                }
-            }
-            catch (Exception exception)
-            {
-                _traceLogger.Error("ReportPortal exception was thrown." + Environment.NewLine + exception);
-            }
+            return _flowItems[id].TestReporter is null
+                ? (_flowItems[id].ParentId.HasValue() ? FindReportedParentFlowItem(_flowItems[id].ParentId) : null)
+                : _flowItems[id];
         }
     }
 }
